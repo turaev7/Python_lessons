37f7270cb86d4bfaceec4966d1ed5495f8e076b2
turaev7 <durbekturayev007@gmail.com>
2022-12-29 10:11:31 +0500

    lesson2-added

---
+++ Exam.py
@@ -0,0 +1,24 @@
+# 1
+# company = ["Apple", "Amazon", "Netflix", "Samsung", "Alibaba", "Ebay"]
+# print(company[5], company[4], company[3], company[2], company[1], company[0])
+
+# 2
+#print(papka_nomi[2], papka_nomi[0])
+
+# 3
+# a = input("Enter your 1st number ")
+# b = input("2nd number: ")
+# c = input("Enter 3rd number: ")
+# d = a + b + c
+# print(sorted(d))
+
+# 4
+# g = [10, 20, 30, 40, 50]
+# gs = []
+# for i in g:
+#     gs.append(i + 10)
+# print(gs)    
+
+# 5
+# num = pow(2, 2, 2)
+# print(num)
--- new_doc.py
+++ new_doc.py
@@ -3,11 +3,20 @@ second = input("Enter your second number: ")
 third = input("Enter your third number: ")
 if first > second > third:
     print(f"sonlarning kattasi {first}\n Eng kichigi {third}")
+    print("o'rta arifmetigi: ", float(first + second + third), "/3")
 elif first > third > second:
     print(f"sonlarning kattasi {first} \n kichigi {second}")
+    print("o'rta arifmetigi: ", float(first + second + third), "/3")
 elif second > first > third:
     print(f"sonlarning kattasi {second} \n kichigi {third}")
+    print("o'rta arifmetigi: ", float(first + second + third), "/3")
 elif second > third > first:
-    print(f"sonlarning kattasi {second} \n kichigi {first}")    
+    print(f"sonlarning kattasi {second} \n kichigi {first}")
+    print("o'rta arifmetigi: ", float(first + second + third), "/3")    
 elif third > first > second:
-    print(f"sonlarning kattasi {third} \n kichigi {second}")    
+    print(f"sonlarning kattasi {third} \n kichigi {second}")
+    print("o'rta arifmetigi: ", float(first + second + third), "/3") 
+elif third > second > first:
+    print(f"sonlarning kattasi {third} \n kichigi {first}")  
+    print("o'rta arifmetigi: ", float(first + second + third), "/3")     
+
