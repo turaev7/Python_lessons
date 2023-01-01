7d7b42b4a2a83e51f15f65a3f002171432c74359
turaev7 <durbekturayev007@gmail.com>
2023-01-01 15:36:33 +0500

    2nd work

---
+++ function.py
@@ -0,0 +1,16 @@
+# funksiya nomi - salom 
+# ( ) - funksiya argumenti
+# print - funksiyaga tegishli bulgan uzgaruvchi
+# def salom():
+#     print("Wassup man")
+# salom()
+
+# def malumot(ism, sharif):
+#     print("Salom hurmatli",ism, sharif)
+
+# malumot("Durbek", "Turaev")   
+
+def bilol(name, surname, age):
+    print("Hi my name is", name, "My surname is", surname, "and I am ", age, "years old")
+
+bilol("Aisha", "Shavkatova", "half")    
