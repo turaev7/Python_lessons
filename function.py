8252f3cd999f5314ed70e9d520a7bb563119b26f
turaev7 <durbekturayev007@gmail.com>
2023-01-05 22:33:22 +0500

    2023

--- function.py
+++ function.py
@@ -27,8 +27,8 @@
 #     print("Assalomu alaykum!")
 
 # salom_ber()
-def salom_ber(ism):
-    """Fodyalanuvchi ismini qabul qilib, 
-        unga salom beruvchi funksiya"""
-    print(f"Assalomu alaykum, hurmatli {ism.title()}!")
-salom_ber("Durbek")    
+# def salom_ber(ism):
+#     """Fodyalanuvchi ismini qabul qilib, 
+#         unga salom beruvchi funksiya"""
+#     print(f"Assalomu alaykum, hurmatli {ism.title()}!")
+# salom_ber("Durbek")    
