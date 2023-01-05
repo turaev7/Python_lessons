458ad3df964c4d21ec27fb07d96c9c7d7f5202fd
turaev7 <durbekturayev007@gmail.com>
2023-01-05 17:39:15 +0500

    2023

---
+++ 2023_1st .py
@@ -0,0 +1,14 @@
+# def talaba(talaba_id, ism_sharif, guruh, kurs,manzil):
+#     print(f" talaba_id:{talaba_id},\n ism_sharif:{ism_sharif} \n guruh: {guruh} \n kurs: {kurs},  manzil: {manzil}")
+# talaba("10099", "Ozod Sharofiddinov", "toq kunlar", "Anatomiya", "Qarshi tuman")    
+
+# def avtomobil(nomi, rangi, mator_kuchi, yuqori_tezligi):
+#     print("Siz so'ragan avtomobil nomi: ",nomi, "rangi: ",rangi, "Matorning kuchi: ",mator_kuchi, "va eng yuqori chiqishi mumkin bo'lgan tezligi:",yuqori_tezligi)
+# avtomobil("Captiva","Oq","2,6hp", "220")
+
+def yosh_hisobla(tugilgan_yil, joriy_yil=2023): # joriy yil uchun st.qiymat 2020
+   
+    print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
+yosh_hisobla(2004)    
+
+
--- function.py
+++ function.py
@@ -10,7 +10,25 @@
 
 # malumot("Durbek", "Turaev")   
 
-def bilol(name, surname, age):
-    print("Hi my name is", name, "My surname is", surname, "and I am ", age, "years old")
+# def bilol(name, surname, age):
+#     print("Hi my name is", name, "My surname is", surname, "and I am ", age, "years old")
 
-bilol("Aisha", "Shavkatova", "half") 
+# bilol("Aisha", "Shavkatova", "half") 
+
+# def get_even_numbers():
+#     for number in range(1, 11):
+#         print(number * 2)
+
+# get_even_numbers()
+
+
+# def salom_ber():
+#     """Salom beruvchi funksiya"""
+#     print("Assalomu alaykum!")
+
+# salom_ber()
+def salom_ber(ism):
+    """Fodyalanuvchi ismini qabul qilib, 
+        unga salom beruvchi funksiya"""
+    print(f"Assalomu alaykum, hurmatli {ism.title()}!")
+salom_ber("Durbek")    
