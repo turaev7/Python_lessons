2ea6098ee631b1feca5b3606746c6080c14ff8d8
turaev7 <durbekturayev007@gmail.com>
2023-01-09 13:42:54 +0500

    lesson

--- function.py
+++ function.py
@@ -32,3 +32,8 @@
 #         unga salom beruvchi funksiya"""
 #     print(f"Assalomu alaykum, hurmatli {ism.title()}!")
 # salom_ber("Durbek")   
+# def get_even_numbers():
+#     for number in range(1, 11):
+#         print(number * 2)
+
+# get_even_numbers()
